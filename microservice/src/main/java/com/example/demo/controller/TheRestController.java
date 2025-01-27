1c253cd2a2b98584b781ac7d301fd448ae39faf6
Yevhen <yevhen.bulatnikov@gmail.com>
2025-01-27 12:43:57 +0200

    eureka server and microservice

---
+++ microservice/src/main/java/com/example/demo/controller/TheRestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TheRestController {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello world from Service A!";
+    }
+
+}
